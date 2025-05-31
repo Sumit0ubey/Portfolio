f9b6a9af5968a36fb98fa9d387297e76facb5dc9
Sumit Dubey <sumit.dubey20317@gmail.com>
2025-05-31 11:31:34 +0530

    Fixed Bugs

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
 let typed = new Typed (".multuple-text", {
     strings: ["Python Developer", "Java Developer", "Android Developer", "Web Developer", "Data Analysis", "data scientist", "Human"],
     typeSpeed: 100,
@@ -15,3 +17,24 @@ function toggleMenu() {
 
 
 document.getElementById("year").textContent = new Date().getFullYear()
+
+
+const linkedin_1 = document.getElementById('linkedin-1');
+const linkedin_2 = document.getElementById('linkedin-2');
+const github = document.getElementById('github');
+const email = document.getElementById('email');
+const achivement_1 = document.getElementById('achivement-1');
+const achivement_2 = document.getElementById('achivement-2');
+const achivement_3 = document.getElementById('achivement-3');
+const education = document.getElementById('education');
+
+if (isDark){
+  linkedin_1.src = './resources/linkedIn-dark.png'
+  linkedin_2.src = './resources/linkedIn-dark.png'
+  github.src = './resources/github-dark.png'
+  email.src = './resources/email-dark.png'
+  education.src = './resources/eduction-dark.png'
+  achivement_1.src = './resources/achievement-dark.png'
+  achivement_2.src = './resources/achievement-dark.png'
+  achivement_3.src = './resources/achievement-dark.png'
+}
